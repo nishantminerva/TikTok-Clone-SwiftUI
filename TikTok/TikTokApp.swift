27137a7ef03654349cb075bc01e938928406ffe0
Nishant <nishantminerva@gmail.com>
2024-03-25 19:49:48 +0530

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Nishant Minerva on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
